0138240fc6ffee23db3b7489c8637047f72c64aa
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-21 23:52:07 +0200

    add num represents

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -3,7 +3,11 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+console.log(`num=${num}`);
+
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//num represents a random integer from minimum to maximum.
