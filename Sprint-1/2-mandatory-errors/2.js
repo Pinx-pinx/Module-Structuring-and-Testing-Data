e7ae2ffe2b04a68058b8ce5c7086a62fc34734e9
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-22 22:06:28 +0200

    declare before logging in console

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,13 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
+
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+// This error occurred because we are trying to  print out a value of a variable that hasn't been declared. 
+// to fix this we can declare the variable first and display the result in the console.
+//correction:
+// const cityOfBirth = "Bolton";
+// console.log(`I was born in ${cityOfBirth}`);
+
