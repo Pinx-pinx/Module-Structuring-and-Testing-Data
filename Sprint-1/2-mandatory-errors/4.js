9e59dd7554a6f5ecda4bd9fbc1a240baae7c4f4b
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-22 22:08:08 +0200

    variable not to start with  a number

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const hour12ClockTime = "20:53";
+const hour24ClockTime = "08:53";
+
+// This code will not work when I run it because a variable shouldn't start with a number
+//To fix this we can change the variable names to hour12ClockTime and hour12ClockTime.
