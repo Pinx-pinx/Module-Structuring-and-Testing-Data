0b177aefa40fd1bf4fa733c8a4667b68a7f4f257
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-22 22:05:29 +0200

    reassign value

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-let age = 33;
-age = 1;
+const age = 33;
+age = age + 1;
+
+// The error here is that we are reassigning the value of age which is declared as a constant variable.
+// to fix this, we ca change the declared variable into "let age" so we will be able to reassign a value.
+// let age =33;
+// age = age +1
