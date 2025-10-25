a976faba182f7924d8db80c4e2c0477847b50e29
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-25 12:35:06 +0200

    fix syntax and ref error

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,24 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
-
+// We will get an error because we've given the function parameter a (literal value) which is 3 and not a (identifier)parameter name so this will give us a syntax error.
+// Next is we are trying to return num * num which has not been declared locally so it will give us an error.
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
+//The errors would be 
+// syntax error and 
+//reference error.
 
-// =============> explain this error message here
-
+// Syntax error- the identifier should be a literal value,it should be parameter name 
+//reference error - the num* num has not been declared locally
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
+//function square(number){
+//return number * number
+//}
+//cosole.log(square(3))
+
 
 
