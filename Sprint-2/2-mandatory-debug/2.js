c3671d0a5e20cf25858f0f6b38db08513c39d873
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-25 14:07:44 +0200

    add parameter,remove cons var

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,7 +1,13 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// In this code we are supposed to get the last digit of the given argument but it will give an error
+//because the variable was declared constant so it will ignore the argument given and also the function getLastDigit was not given a parameter so it will not take any argument 
+// return num.toString().slice(-1); - .slice works on strings and arrays, so here we convert the number to a string so that .slice(-1) works
+//the output you will always get is 
+//The last digit of 42 is 3
+//The last digit of 105 is 3
+//The last digit of 806 is 3
 
 const num = 103;
 
@@ -14,11 +20,22 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// I run the code and the output is the same as my prediction 
+// the   output you will always get is 
+//The last digit of 42 is 3
+//The last digit of 105 is 3
+//The last digit of 806 is 3
 // Explain why the output is the way it is
-// =============> write your explanation here
+// The output is the way it is because first there was a costant variable declared which 103
+// Then also the function was not give a parameter name so it will not take any argumentt.
 // Finally, correct the code to fix the problem
-// =============> write your new code here
+//  function getLastDigit(num) {
+//  return num.toString().slice(-1);
+//  }
+
+//console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+//console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+//console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
