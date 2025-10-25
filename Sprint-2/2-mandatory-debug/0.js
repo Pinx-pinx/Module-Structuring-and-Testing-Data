76265c73b0c458fc2749b68b6a6b0924ef1a234e
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-25 13:25:05 +0200

    add explanation

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// In this code we are supposed to have a result of multiplying a and b value but there are errors in tthis code.
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,16 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// This function takes 2 parameters and logs the product of a * b inside the function.
+// console.log(a * b) will print the product immediately inside the function, 
+// but it does not return a value to the caller. 
+// Therefore, using multiply(10, 32) inside a template literal shows undefined, 
+// because the function must return a value for the template literal to display it.
+/
+/ Finally, correct the code to fix the problem
+ // function multiply (a,b){
+ 
+  //  return a*b;
+  // }
 
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+  // console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
