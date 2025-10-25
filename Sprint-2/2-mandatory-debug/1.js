8c1ef60119b15901974ae244168094e7753f9a31
Pinx-pinx <pinkybenitez3364@gmail.com>
2025-10-25 13:40:12 +0200

    explain and fix error

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  This code should return a value of adding a and b but this code has an error
 
 function sum(a, b) {
   return;
@@ -8,6 +8,15 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+// The function sum takes 2 parameter
+//return; was used as statement without  a value so the return is undefined.
+//a+b; will not run because it comes after return
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); using th function as a template literal will give you undefined since 10+32 was never executed
+
+// // Finally, correct the code to fix the problem
+
+// function sum(a,b){
+
+//  return a+b
+// }
+ // console.log(`The sum of 10 and 32 is ${sum(10, 32)}`)
