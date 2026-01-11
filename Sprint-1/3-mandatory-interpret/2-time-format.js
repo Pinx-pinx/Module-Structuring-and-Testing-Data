77e1ef9894cc7f91f361c9b40527b0b4c26a2b41
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-12 00:40:23 +0100

    change answer

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -20,16 +20,15 @@ console.log(result);
 // 5. const totalHours = (totalMinutes - remainingMinutes) / 60;
 // 6. const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 
-
 // b) How many function calls are there?
-// answer: Ther are no function calls in the code.
+// answer: Ther is 1 function call in the code which is the console.log
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 //answer: the expression movieLength % 60 used a modulus operator %
 // This % will give the remainder of the division operation.
 //In this line movieLenghth is (8784). when we divide this to 60 it will give us 146.4.
-// Multiply 146 to 60 it will give us 8760. 
+// Multiply 146 to 60 it will give us 8760.
 // Then if we substract this 8760 from 8784 it will give us 24 as the remaining seconds
 //so the expression movieLength % 60 represents the remaining seconds.
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
@@ -42,7 +41,7 @@ console.log(result);
 //movieDuration would be the better name of this varible.
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
 // Answer: I tried experimenting with different values like negative number, decimal number and even 0 and it did not break the code.
-  // 1. Integers will work perfectly and return you a meaningful result.
-  // 2. negative numbers will work but it will also gve you a negative result which is not acceptable for time calculation as it will return you a meaning lesst result.
-  // 3. Decimal number works fine as well but not also acceptable in time calculation as it will not give a precise floating point numbers.
-  // 4. 0 will work but will always return a 0 so its not also acceptable  for time calculation because it will return a meaningless result
+// 1. Integers will work perfectly and return you a meaningful result.
+// 2. negative numbers will work but it will also gve you a negative result which is not acceptable for time calculation as it will return you a meaning lesst result.
+// 3. Decimal number works fine as well but not also acceptable in time calculation as it will not give a precise floating point numbers.
+// 4. 0 will work but will always return a 0 so its not also acceptable  for time calculation because it will return a meaningless result
